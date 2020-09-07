1027702a589a9505259bbac613dc25df3d2d6e8b
Ofrepose <paynedanielfranklin@gmail.com>
2020-09-06 20:20:30 -0400

    completed

--- data/db.js
+++ data/db.js
@@ -2,4 +2,27 @@ const knex = require("knex");
 const config = require("../knexfile");
 const db = knex(config.development);
 
-module.exports = db;
+module.exports = {
+    createUser,
+    findByUsername,
+    getAllUsers,
+};
+
+
+
+function createUser(user){
+    return db('users')
+        .insert(user)
+        .then(userData => userData)
+}
+
+function findByUsername(user){
+    console.log(`username in db is ${user.username}`);
+
+
+    return db('users').where('username',user.username);
+}
+
+function getAllUsers(){
+    return db('users').select('*')
+}
--- server.js
+++ server.js
@@ -8,10 +8,19 @@ const bcrypt = require("bcryptjs");
 
 const session = require("express-session");
 
+async function protected(req,res,next){
+    console.log(`inside protected the req.session.username is ${req.session.user} and the req.session is ${req.session}`)
+    if(req.session && req.session.user){
+        next();
+    }else{
+        res.status(401).json({message:'You shall not pass!'})
+    }
+}
+
 server.use(
     session({
         name: 'notsession',
-        secret:'nobody tosses a dwarf',
+        secret:'nobody tosses a dwarf!',
         cookie: {
             maxAge: 1 * 24 * 60 * 60 * 1000,
             secure: true,
@@ -30,6 +39,37 @@ server.listen(port, ()=>{
 })
 
 server.post('/api/register', (req,res)=>{
-    req.
+    const credentials = req.body;
+    const hash = bcrypt.hashSync(credentials.password, 14);
+    credentials.password = hash;
+    db.createUser(credentials)
+        .then(x=>{
+            res.status(200).json({message:"User created"})
+        })
+        .catch(err=>{res.status(500).json({message: err})});
+})
+
+server.post('/api/login', (req, res)=>{
+    let {username, password} = req.body;
+    console.log({username});
+    console.log({password});
+    db.findByUsername({username})
+        .first()
+        .then(user=>{
+            console.log(user.password)
+            if(user && bcrypt.compareSync(password, user.password)){
+                req.session.user= user.username
+                res.status(200).json({message: 'logging in', cookie:req.session.id})
+            }else{
+                res.status(401).json({message:'You shall not pass!'})
+            }
+        })
+        .catch(err=>{res.status(500).json({message:err})})
+})
+
+server.get('/api/users', protected, (req,res)=>{
+    db('users')
+        .then(users=>res.json(users))
+        .catch(err=> res.json(err))
 })
 
